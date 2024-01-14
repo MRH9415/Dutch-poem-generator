8b1c10f7507265f0be30a47189c103b0904443c3
MRH9415 <149942702+MRH9415@users.noreply.github.com>
2024-01-14 11:44:38 +0100

    Adding JS AI into the form

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-  new Typewriter("#poem", {
-    strings: "In holland staat een huis",
+function displayPoem (response){
+    new Typewriter("#poem", {
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector ("user-instructions")
+let apiKey = "c9b0ec623aacc60coeb8df5a545t1489";
+let prompt = 'Generate a Dutch Poem about ${instructionsInput.value}';
+let context ="User instructions are: You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML.Make sure to follow the user instructions";
+let apiURL = 'https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}' 
+
+console.log ("Generating poem");
+console.log('prompt: $ {prompt}');
+console.log('context: $ context}');
+
+axios.get(apiURL).then(displayPoem);
 
 let poemFormElement = document.querySelector("poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
